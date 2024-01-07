0e26d5fcf488a72cbd3de26ff02088b6fe0e3285
Ajay Menon <77717855+ajay-menon-iitkgp@users.noreply.github.com>
2024-01-07 17:46:48 +0800

    Clean up BemioParseScript file

--- BemioParseScript.m
+++ BemioParseScript.m
@@ -1,6 +1,6 @@
-PlateBEM.FexcRe = h5read('D:\Mitacs Globalink 2022\Sys-MoDEL\Modelica Codes\rm3.h5','/body1/hydro_coeffs/excitation/components/re/3_1');
+PlateBEM.FexcRe = h5read('D:\...\rm3.h5','/body1/hydro_coeffs/excitation/components/re/3_1');
 PlateBEM.FexcRe = PlateBEM.FexcRe(1,:);
-PlateBEM.FexcIm = h5read('D:\Mitacs Globalink 2022\Sys-MoDEL\Modelica Codes\rm3.h5','/body1/hydro_coeffs/excitation/components/im/3_1');
+PlateBEM.FexcIm = h5read('D:\...\rm3.h5','/body1/hydro_coeffs/excitation/components/im/3_1');
 PlateBEM.FexcIm = PlateBEM.FexcIm(1,:);
-PlateBEM.w = h5read('D:\Mitacs Globalink 2022\Sys-MoDEL\Modelica Codes\rm3.h5','/simulation_parameters/w');
+PlateBEM.w = h5read('D:\...\rm3.h5','/simulation_parameters/w');
 save('PlateBEM.mat')
